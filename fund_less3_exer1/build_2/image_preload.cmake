d92cc9ca36b7dd12cf44d6be240d48a282081244
Ye Zhou <u1220084@utah.edu>
2023-02-14 15:58:01 -0600

    Follow the tutorial
    Finished Lesson 3

---
+++ fund_less3_exer1/build_2/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/zhouye/nordic/myApps/nRF-Connect-SDK-Fundamentals/v2.x.x/lesson3/fund_less3_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "/Users/zhouye/nordic/myApps/nRF-Connect-SDK-Fundamentals/v2.x.x/lesson3/fund_less3_exer1/nrf5340dk_nrf5340_cpuapp.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/v2.2.0/bin/python3" CACHE INTERNAL "NCS child image controlled")
