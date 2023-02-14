f48584f7b74ed1b2799961b6096e53d38706ecb2
Ye Zhou <u1220084@utah.edu>
2023-02-14 16:25:58 -0600

    Finished lesson 4.
    Printk() and logger module features.

---
+++ fund_less4_exer3/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/zhouye/nordic/myApps/nRF-Connect-SDK-Fundamentals/v2.x.x/lesson4/fund_less4_exer3/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/v2.2.0/bin/python3" CACHE INTERNAL "NCS child image controlled")
